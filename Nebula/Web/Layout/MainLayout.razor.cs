e1189b03fa10f3e3b61617892d30be9df1a5b493
Byrone <byronoud10@gmail.com>
2025-07-14 11:58:02 +0200

    mobile navigation

--- Nebula/Web/Layout/MainLayout.razor.cs
+++ Nebula/Web/Layout/MainLayout.razor.cs
@@ -1,10 +1,28 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Extensions.Localization;
+using Nebula.Services;
 
 namespace Nebula.Web.Layout
 {
 	public sealed partial class MainLayout : LayoutComponentBase
 	{
+		private const string signOutFormName = "logout";
+
 		[Inject] public required IStringLocalizer<MainLayoutLocalization> Localizer { get; init; }
+
+		[Inject] public required AuthenticationService Authentication { get; init; }
+
+		[Inject] public required NavigationManager Navigation { get; init; }
+
+		private readonly EditContext context = new(0);
+
+		private async Task SignOutAsync()
+		{
+			await this.Authentication.DeauthenticateAsync();
+
+			this.Navigation.NavigateTo("/login", true);
+		}
 	}
 }
